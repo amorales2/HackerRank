a80e11ce371fa369cd5aeaac5039174c9c8df935
Andrew Morales <amorales2@live.com>
2016-02-19 19:25:29 -0600

    Added DijkstraShortestReach project

---
+++ DijkstraShortestReach2/DijkstraShortestReach2/main.cpp
@@ -0,0 +1,37 @@
+﻿///////////////////////////////////////////////////////////////////////////////
+//
+//main.cpp
+//
+
+
+/*
+Given a graph consisting NN nodes (labelled 1 to N) where a specific given 
+node SS represents the starting position SS and an edge between two nodes is 
+of a given length, which may or may not be equal to other lengths in the graph.
+
+It is required to calculate the shortest distance from the start position 
+(Node S) to all of the other nodes in the graph.
+
+Note 1: If a node is unreachable , the distance is assumed as −1−1.
+
+///////////////////////////////////////////////////////////////////////////////
+Input Format
+
+The first line contains TT, denoting the number of test cases.
+First line of each test case has two integers NN, denoting the number of nodes 
+in the graph and MM, denoting the number of edges in the graph.
+
+The next MM lines each consist of three space-separated integers xx yy rr, where
+xx and yy denote the two nodes between which the undirected edge exists, rr 
+denotes the length of edge between these corresponding nodes.
+
+The last line has an integer SS, denoting the starting position.
+*/
+
+#include <iostream>
+
+int main()
+{
+
+	return 0;
+}
